b5c807cad45f9c9edcc7ea136f4530428f86ea3c
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-11 06:44:54 -0800

    testing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - print type
+ *
  * return: 0 if 
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
-	return (0);
+	return(0);
 }
