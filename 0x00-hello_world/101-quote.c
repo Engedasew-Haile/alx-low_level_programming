7feec9fd7cadd9dc56c8a1d4606af4e548114d25
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-11 07:54:58 -0800

    Testing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful" 
- * - Dora Korpar, 2015-10-19",
+ *main - prints exactly "and that piece of art is useful"
+ *- Dora Korpar, 2015-10-19",
  * followed by a new line, tobthe standard error.
- * Return: 1 always- 0 
+ *Return: 1 always
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
