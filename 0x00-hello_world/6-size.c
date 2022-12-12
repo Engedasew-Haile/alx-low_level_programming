450afe2cfd9131c2be1f66fabf82cec62b153e79
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-12 12:50:08 -0800

    testing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@ int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of long: %d byte(s)\n", sizeof(long));
 	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
