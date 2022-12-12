df50dc5c34879ac455506b18c59abd7997cb6b72
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-12 12:35:50 -0800

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
  * main - print type
- * Return: 0 if exited properly, non-zero 
+ * Return:0 if exited properly, non-zero 
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
