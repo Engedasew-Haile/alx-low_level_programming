0a4c1e19fe7c0449486232bc2d64f9545ed9e4d4
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-13 10:10:49 -0800

    testing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,3 @@ int main(void)
 	printf("Size of float: %.6d byte(s)\n",sizeof(float));
 	return (0);
 }
-/**
- * Size of a char: 1 byte(s)
- * Size of an int: 4 byte(s)
- * Size of a long int: 8 byte(s)
- * Size of a long long int: 8 byte(s)
- * Size of a float: 4 byte(s)
- */
