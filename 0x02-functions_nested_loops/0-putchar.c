ea976c85e662daa5d6e5f2f1ab2d78c5151c15e5
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-16 09:56:38 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
 #include "mian.h"
 /**
- * _putchar - writer 
+ *main - entry point 
  *
- * Return: -1
+ * Return: 0
  */
-int _putchar(char c)
+int main(void)
 {
-	return (writr(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
