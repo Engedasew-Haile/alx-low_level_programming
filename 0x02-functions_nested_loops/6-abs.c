45d97d5264588d9ac732b14a19c8307999b54e50
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-17 13:02:15 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - a function that compute abs value of an int
+ * @ab: integer input
+ * Return: abs value of ab
+ */
+int _abs(int ab);
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
