2b3be0e9a7577f1f25d552943a1879787aa81d08
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-18 14:28:44 -0800

    an int is postitive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function an integer is + or -
+ * Return: 0 always
+ */
+
+inat main(void)
+{
+	int j;
+
+	j = 0;
+	positive_or_negative(j);
+	return (0);
+}
