4d57845d5d7602d2871c287282e240b4568120a8
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-18 14:43:50 -0800

    an int p+ or neg-

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,10 +6,20 @@
 
 inat main(void)
 {
-	int i;
+	int n;
 
-	i = 98;
-	positive_or_negative(i);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+		printf("%d is zero\n", n);
 
 	return (0);
 }
