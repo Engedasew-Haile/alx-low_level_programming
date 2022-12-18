d177b57b9f28856db4d871cf1ae429f65d0e6cfd
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-18 11:57:15 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print the sum of even Fibonacci numbers up to a fib value
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1, j = 2, total = 0;
+	int k;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			tot += j;
+
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%d\n", total);
+	return (0);
+}
