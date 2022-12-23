e0115aeaa6da89fd904c1728a00fe61c32a5dfb8
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-23 08:50:11 -0800

    update something

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,20 +3,19 @@
  * reverse_array - prints reverse array
  * @a: array to be compared
  * @n: size of array
- * Return: reversed array
  */
 void reverse_array(int *a, int n)
 {
-        int start_point, swap, end;
+	int swap, start, end;
 
-        start_point = 0;
-        end = n - 1;
-        while (start_point < end)
+	start = 0;
+	end = n -1;
+	while (start < and)
 	{
-		swap = *(a + start_point);
-		*(a + start_point) = *(a + end);
+		swap = *(a +start);
+		*(a + start) = *(a + end);
 		*(a + end) = swap;
-		start_point++;
+		start++;
 		end--;
 	}
 }
