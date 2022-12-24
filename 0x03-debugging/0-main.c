cfbe6bffd47ef68b0e18868b49b2afb318fa4a83
Engedasew-Haile <engedasewhaile@gmail.com>
2022-12-24 07:23:38 -0800

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,20 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+void positive_or_negative(int i)
+{
+int main(void)
+{
 /**
  * main - tests function an integer is + or -
+ * positive_or_negative - test for + or -
+ * Description: i think iy may need here
  * Return: 0 always
  */
 
-inat main(void)
-{
+int main(void)
+
 	int n;
 
 	srand(time(0));
@@ -23,6 +29,20 @@ inat main(void)
 	}
 	else
 		printf("%d is zero\n", n);
+
+	return (0);
+}
+}
+/**
+ * main - tests function an integer is + or -
+ * Return: 0 always
+ */
+int main(void)
+{
+	int j;
+
+	j = 0;
+	positive_or_negative(j);
 
 	return (0);
 }
