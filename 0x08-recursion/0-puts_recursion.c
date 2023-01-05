76c1c24e812815d2ed0679edaf4b8ad9a56ef26f
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-04 17:06:54 -0800

    Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,21 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string
- * @s: the pointer input
+ * _puts_recursion - prints a string trhough recursion
+ * @s: char
+ *
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(++s);
+	else if (!*s)
+	{
+	_putchar('\n');
+	return;
+	}
 }
