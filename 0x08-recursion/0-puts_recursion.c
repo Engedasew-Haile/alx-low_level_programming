b3c0978e2ef76c6a4581a6852d23dbe7a71ad7ab
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-04 17:34:11 -0800

    Task 0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string using recursion
+ * @s: char
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+}
