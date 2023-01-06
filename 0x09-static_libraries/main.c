bb5dbf6064ea2f86b2fd17c66638ea43077a20d7
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-01-07 01:37:09 +0300

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,4 @@
-#ifndef main_H
-#define main_H
+#!/bin/bash
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
