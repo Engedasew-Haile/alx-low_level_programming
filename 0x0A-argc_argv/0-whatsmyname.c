691bbe8559ec6af22a7a72d6cca6e11ebaf68c5b
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-07 05:44:16 -0800

    Task 0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print its name flow \n
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+		return (0);
+}
