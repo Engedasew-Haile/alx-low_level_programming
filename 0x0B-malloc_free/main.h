a24a133df760ba03f19ef57da04da675938fa1ee
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-11 09:36:49 -0800

    update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
