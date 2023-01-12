d9d92299ec24c822a48593af2b8f1f08337b3330
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-12 08:42:37 -0800

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,23 +16,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int a, b, c, d;
 
 	if (s1 == 0)
+	{	
 		s1 = "";
+	}
 	if (s2 == 0)
+	{
 		s2 = "";
 		for (a = 0; s1[a] != '\0'; a++)
+		{
 		;
+		}
 		for (b = 0; s2[b] != '\0'; b++)
+		{
 		;
+		}
+	}
 	if (n > b)
+	{
 		k = malloc((a + b + 1) * sizeof(char));
+	}
 	else
 		k = malloc((a + n + 1) * sizeof(char));
 	if (k == 0)
+	{
 		return (NULL);
+	}
 	for (c = 0; c < a; c++)
+	{
 		k[c] = s1[c];
+	}
 	for (d = 0; d < n && d < b; d++, c++)
+	{
 		k[c] = s2[d];
+	}
 	k[c] = '\0';
 	return (k);
 }
