5ee27a9fdb416c8b540fc5b5101f74c51b79f018
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-18 00:53:16 -0800

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,18 @@
  * Description: defines dog type
  */
 
-typdef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
