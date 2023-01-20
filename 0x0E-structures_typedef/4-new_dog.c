ff302d89ad0d464525a6ae1ad74259bae7b61f1d
Engedasew-Haile <engedasewhaile@gmail.com>
2023-01-20 01:59:10 -0800

    Task 3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - entry point
+ * @name: str name of pet
+ * @age: age of pet
+ * @owner: str owner of pet
+ * Return: p
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	if (name == NULL)
+	{
+		free(p);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(p);
+		free(name);
+		return (NULL);
+	}
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
