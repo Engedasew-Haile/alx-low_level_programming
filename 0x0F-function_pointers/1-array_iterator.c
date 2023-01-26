00633170bc7c3befd3f73139f5eb93bdfd2b60a4
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-01-27 00:13:42 +0300

    Creating  more files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_elem - prints an integer
+ * @elem: the integer to print
+ * Return: Nothing.
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+     size_t i;
+
+     for (i = 0; i <size; i++)
+     action(array[i]);
+}
