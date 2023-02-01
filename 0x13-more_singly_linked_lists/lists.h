c8811a37dd701388e7b78ddbc138f7f327c74039
Engedasew-Haile <engedasewhaile@gmail.com>
2023-02-01 08:22:16 -0800

    update both

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+size_t listint_len(const listint_t *h);
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
