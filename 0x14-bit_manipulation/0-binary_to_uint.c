b7d9f254225235b4c790a3d874b7285d97081bf8
Engedasew-Haile <engedasewhaile@gmail.com>
2023-02-03 14:34:20 -0800

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b) /*binary_to_uint - text gose here */
 
 	for (len -= 1; len >= 0; len--)
 	{
-		if (b[len] != 0 && b[len] != 1)
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
 		num += (b[len] - 0) * mult;
