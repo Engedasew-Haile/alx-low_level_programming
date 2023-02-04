9fbea69d9b01e19b0b16592dae88caec25158885
Engedasew-Haile <engedasewhaile@gmail.com>
2023-02-04 07:16:11 -0800

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
 /**
  * print_listint - prints all the elements
  * @h: linked list of type listint_t to prin
- * Return: number of nod
+ * Return: number of nod.
  */
 
 int size_t print_listint(const listint_t *h)
