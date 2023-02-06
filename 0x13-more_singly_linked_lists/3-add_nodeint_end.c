43b05c0d5c8f9b7e424eed0bd81f1ff6b5f1b026
Engedasew-Haile <engedasewhaile@gmail.com>
2023-02-06 14:04:57 -0800

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  * Return: pointer to the new node, or NULL if it fails
  */
 
-lisint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 
