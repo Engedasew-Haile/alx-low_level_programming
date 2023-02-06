eb001540190ceac72d54139eb8e1990329a36bbb
Engedasew-Haile <engedasewhaile@gmail.com>
2023-02-06 14:12:59 -0800

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,13 +9,9 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *check, *c;
-
-	c = head;
-	while (c != NULL)
+	if (head)
 	{
-	check = c->next;
-	free(c);
-	c = check;
+		free_listint(head->next);
+		free(head);
 	}
 }
