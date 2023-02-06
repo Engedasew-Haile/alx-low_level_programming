365b701522cc1210138484b9c5eda70bda970f90
Engedasew-Haile <engedasewhaile@gmail.com>
2023-02-06 07:58:45 -0800

    update 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,6 +6,7 @@
  * read_textfile - that reads a text file and prints
  * @filename: variable pointer
  * Description: Write a function that reads a text file and prints it
+ * @letters: des will ber here
  * Return: the actual number of letters it could read and print, 0 otherwise
  */
 
