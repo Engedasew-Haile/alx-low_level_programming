5285554f7bed802cad8b7f62d691bf586200e8c2
Engedasew-Haile <engedasewhaile@gmail.com>
2023-03-05 03:28:42 -0800

    task 10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/*
+/**
  * delete_nodeint_at_index - delete node at index
  * @head: head node
  *
@@ -36,7 +36,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	return (1);
 }
 
-/*
+/**
  * get_nodeint_at_index - get node at index
  * @head: head node
  * @index: index node
