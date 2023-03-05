789fdf490dde86f704261f1b60c48bbe7e9c4cd2
Engedasew-Haile <engedasewhaile@gmail.com>
2023-03-05 06:18:59 -0800

    task 0. What's my name, update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,7 @@
  * print_name - Print a name from a function pointer
  * @name: char string
  * @f: function pointer that takes a string argument
- * Return: this is a void function no return
+ *
  */
 
 void print_name(char *name, void (*f)(char *))
