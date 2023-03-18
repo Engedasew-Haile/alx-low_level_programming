68f728d1d409d9dd0280ecff5af3178806b45e7e
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-03-18 14:02:38 +0300

    Update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,12 @@
 #include "lists.h"
+#inclide <stdio.h>
+
 /**
  * print_dlistint - Prints all the nodes of ll
  * @h: Is the pointer to node structure
  * Return: the amount of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int node_count = 0;
@@ -16,4 +19,3 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 	return (node_count);
 }
-
