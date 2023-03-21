02f20178742a13c2ef6c89b47539990230464fdb
Engedasew Haile <117769482+Engedasew-Haile@users.noreply.github.com>
2023-03-21 11:57:53 +0300

    Update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint- Insert new node at the beginnig
+ * add_dnodeint- Insert new node at the beginnig last test
  * @head: Is the pointer to the direction of the head
  * @n:The data integer
  * Return: The new node
