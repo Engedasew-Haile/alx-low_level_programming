63da124ec036e8c6da1a1ca5e8c6ecbab83b33d8
Engedasew-Haile <engedasewhaile@gmail.com>
2023-03-21 02:08:31 -0700

    update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -5,22 +5,5 @@
  * @n:The data integer
  * Return: The new node
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new_mem;
 
-	new_mem = malloc(sizeof(dlistint_t));
 
-	if (new_mem == NULL)
-		return (NULL);
-
-	new_mem->n = n;
-	new_mem->prev = NULL;
-	new_mem->next = *head;
-
-	if (*head != NULL)
-		(*head)->prev = new_mem;
-	*head = new_mem;
-
-	return (new_mem);
-}
