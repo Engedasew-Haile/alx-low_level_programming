1962d36b49302538d9e951bb5499a38594771363
Engedasew-Haile <engedasewhaile@gmail.com>
2023-04-09 20:39:16 -0700

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,9 +3,9 @@
 /**
  * hash_table_create - Create the hash table.
  * @size: Size of the table.
- *
  * Return: The hash table.
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
